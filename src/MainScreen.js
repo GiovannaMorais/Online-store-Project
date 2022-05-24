7d633f259fb9c1015cbbbf8699c42c128ead2f79
Gabriel Scott <gabrielpcreis@gmail.com>
2022-05-24 15:59:21 -0300

    Componente criado

---
+++ src/MainScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class MainScreen extends React.Component {
+  render () {
+      return (
+          <div>
+              <label htmlFor="busca">
+                <input
+                  type="text"
+                />    
+              </label>
+              <p data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</p>
+          </div>
+      )
+  }
+}
