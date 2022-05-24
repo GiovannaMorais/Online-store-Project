13cdd5723fbe41e0f0b1a6396e0a332e3ae4d330
Gabriel Scott <gabrielpcreis@gmail.com>
2022-05-24 17:27:21 -0300

    requisito 3

--- src/MainScreen.js
+++ src/MainScreen.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class MainScreen extends React.Component {
   render() {
@@ -10,6 +11,9 @@ class MainScreen extends React.Component {
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        <Link to="ShoppingCart">
+          <button type="submit" data-testid="shopping-cart-button">Carrinho</button>
+        </Link>
       </div>
     );
   }
