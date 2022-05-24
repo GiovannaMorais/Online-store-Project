c543373d602c2eb3c4ac4bef094d2c9c99ec43ef
VictorPagotto <victor.pagotto12@gmail.com>
2022-05-24 18:03:39 -0300

    Arrumado 2.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import './App.css';
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 import MainScreen from './MainScreen';
-import Categories from './components/Categories';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={ MainScreen } />
-        <Route exact path="/categories" component={ Categories } />
       </Switch>
     </Router>
   );
