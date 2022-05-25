cc1eb0a6303dcd91ca80f5ffc81fa983212b798d
Fernanda Vidal <fevidal.dev@gmail.com>
2022-05-25 15:32:38 -0300

    enviando componente.

--- src/pages/DetailsProduct.jsx
+++ src/pages/DetailsProduct.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
 class DetailsProduct extends React.Component {
-    
+    const { match: { params: { id } } } = this.props;
+    render() {
+        return(
+
+        )
+    }
 }
