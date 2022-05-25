857f6273261f0a3cd21741ed7fa4b9ecba1dad8f
Fernanda Vidal <fevidal.dev@gmail.com>
2022-05-25 16:02:35 -0300

    reenviando componente.

--- src/pages/DetailsProduct.jsx
+++ src/pages/DetailsProduct.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import propTypes from 'prop-types';
+import * as api from '../services/api';
 
 export default class DetailsProduct extends React.Component {
-  render() {
+  async componentDidMount() {
     const { match: { params: { id } } } = this.props;
+    const productInfo = await fetch(`https://api.mercadolibre.com/items/${id}`)
+      .then((response) => response.json());
+  }
+
+  render() {
     return (
       <h1>Product details</h1>
+
     );
   }
 }
+
+DetailsProduct.propTypes = {
+  match: propTypes.shape({
+    params: propTypes.shape({
+      id: propTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
