0865dca9eb2c34cb9067a96ad7ea4545ba93fed2
GiovannaMorais <gigimorais9@hotmail.com>
2022-05-26 16:33:00 -0300

    Requisito 12 feito

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import CardProductsToBuy from './components/CardProductsToBuy';
-// import CartCheckout from './components/CartCheckout';
 
 class ShoppingCart extends React.Component {
   render() {
