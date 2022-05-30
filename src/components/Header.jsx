7f5cc3239e7fab67cc25d8442c48a1105b1c07a9
Fernanda Vidal <fevidal.dev@gmail.com>
2022-05-30 19:43:45 -0300

    Adiciona Header e CSS.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,44 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Carrinho from '../image/Carrinho.png';
+import ToShoppingCartButton from './ToShoppingCartButton';
+import '../css/header.css';
 
 class Header extends React.Component {
-  render() {
-    return (
-      <nav>
-        <ul>
-          <li>
-            <Link to="/ShoppingCart">
-              <img src="https://cdn-icons-png.flaticon.com/512/126/126510.png" alt="Shopping Cart" />
-              Home
+    update = () => {
+      this.forceUpdate();
+    }
+
+    readsQuantity() {
+      if (sessionStorage.productsToBuy === undefined) {
+        sessionStorage.setItem('productsToBuy', JSON.stringify([]));
+      }
+      const productsToBuy = JSON.parse(sessionStorage.getItem('productsToBuy'));
+      return productsToBuy.length;
+    }
+
+    render() {
+      return (
+        <div className="header">
+          <div className="title">
+            <Link className="link" to="/">
+              <h1>DarkMarket</h1>
             </Link>
-          </li>
-        </ul>
-      </nav>
-    );
-  }
+          </div>
+          <nav>
+            <Link to="/ShoppingCart">
+              <div className="divQuantity">
+                <img src={ Carrinho } alt="carrinho" />
+                <ToShoppingCartButton
+                  quantity={ this.readsQuantity() }
+                  screen={ () => this.update() }
+                />
+              </div>
+            </Link>
+          </nav>
+        </div>
+      );
+    }
 }
 
 export default Header;
